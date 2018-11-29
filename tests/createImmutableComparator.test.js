3d1fdadb586c92160f04cbd122d9d729c13dad59
Мансуров Дмитрий <dmitry.mansurov@gxbventures.com>
2018-11-29 17:08:24 +0300

    test for sort ordered map

--- tests/createImmutableComparator.test.js
+++ tests/createImmutableComparator.test.js
@@ -1,4 +1,5 @@
-import { wrap, createImmutableComparator } from '../src';
+import {wrap, createImmutableComparator} from '../src';
+import {OrderedMap} from "immutable";
 
 test('immutable comparator asc', () => {
     const list = wrap([{a: 2}, {a: 1}, {b: 2}]);
@@ -8,6 +9,32 @@ test('immutable comparator asc', () => {
 
 test('immutable comparator desc', () => {
     const list = wrap([{a: 1}, {a: 2}, {b: 2}]);
-    expect(list.sort(createImmutableComparator('a', {nullsLast: true, desc: true})).toJS()).toEqual([{a: 2}, {a: 1}, {b: 2}]);
-    expect(list.sort(createImmutableComparator('a', {nullsLast: false, desc: true})).toJS()).toEqual([{b: 2}, {a: 2}, {a: 1}]);
+    expect(list.sort(createImmutableComparator('a', {
+        nullsLast: true,
+        desc: true,
+    })).toJS()).toEqual([{a: 2}, {a: 1}, {b: 2}]);
+    expect(list.sort(createImmutableComparator('a', {
+        nullsLast: false,
+        desc: true,
+    })).toJS()).toEqual([{b: 2}, {a: 2}, {a: 1}]);
+});
+
+test('ordered map sorting', () => {
+    const map = (new OrderedMap()).merge({
+        'a': {
+            orderIndex: 0
+        },
+        'd': {
+            orderIndex: null
+        },
+        'c': {
+            orderIndex: 2
+        },
+        'b': {
+            orderIndex: 1
+        },
+    });
+    expect(map.keySeq().toArray()).toEqual(['a', 'd', 'c', 'b']);
+    const sortedMap = map.sort(createImmutableComparator('orderIndex', {nullsLast: true}));
+    expect(sortedMap.keySeq().toArray()).toEqual(['a', 'b', 'c', 'd']);
 });
